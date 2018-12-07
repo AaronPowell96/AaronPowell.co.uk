dcaff31676f283e812bf5cbefd9ae0fdfa725aca
Aaron Powell <Apowell829@gmail.com>
2018-12-07 17:40:30 +0000

    Edited how cvModal displays

--- modals.php
+++ modals.php
@@ -17,9 +17,9 @@
 <p> 
     <ul>
         <li>Achieved a high Distinction (80%) in the first year.</li>
-        <li>96% in Object Orientated Programming, as an introduction to java I gained the fundamentals I needed to understand programming and I am moving forward with an open mind to all languages, front-end or back-end!</li>
-        <li>90% in Databases and the Web. Learning that JavaScript can be used server side excited me about the use of libraries and the possibilities of programming languages.</li>
-        <li>The software engineering module exposed me to agile development, individually and in groups. Carrying out presentations in group meetings to discuss and understand customer requirements. We used UML as a tool to understand real world operations, interpreting and communicating with users to create their perfect product.</li>
+        <li>96% in Object Orientated Programming, as an introduction to Java I gained the fundamentals I needed to understand programming and I am moving forward with an open mind to all languages, front-end or back-end!</li>
+        <li>90% in Databases and the Web. •	Databases and the Web taught the basics of JavaScript, increasing the user’s interaction on a website with JavaScript features, as one of my interests I am currently teaching myself how JavaScript can be used at a higher level with React.</li>
+        <li>The Software Engineering module has exposed me to the use of agile development, individually and in groups. Working in a fast-paced environment, enhancing my ability to adapt to changes, and learn on the go.</li>
         <li>Other first year grades: Computer Systems – 83%, Foundations of Computing I – 79%, Further OOP – 79%, Foundations of Computing II – 77%, People and Computing – 68%, 
 Human Computer Interaction – 66%.
         </li>
@@ -28,9 +28,7 @@ Human Computer Interaction – 66%.
         <span class="subtitle3">ACCESS TO HIGHER EDUCATION, MATHS & PHYSICS | 2017 | COLCHESTER INSTITUTE </span>
 <p>
     <ul>
-        <li>Achieving 9 Distinctions and 36 Merits at level 3 (A level) allowing me to progress to university.
-        The course provided me with the foundations of learning, before learning I first had to learn how to learn,
-        including handling the pressure of fast paced deadlines and still handing in high quality work.</li>
+        <li>Achieving 9 Distinctions and 36 Merits at level 3 (A level) allowing me to progress to university. The course provided me with the foundations of learning, before learning at such a fast pace I first had to learn how to learn efficiently, including handling the pressure of frequent deadlines and still handing in high quality work.</li>
 </ul>
 </p>
 
@@ -40,9 +38,9 @@ Human Computer Interaction – 66%.
          <li>Fully Qualified with Level 3: 2365 17th Edition in Electrical Installations</li>
          <li>Including but not limited to:
              <ul>
-             <li>Principles of electrical science</li>
-             <li>Inspection, Testing, Fault diagnostics and rectification</li>
-             <li>Electrical systems design</li>
+             <li>Principles of Electrical Science</li>
+             <li>Inspection, Testing, Fault Diagnostics and Rectification</li>
+             <li>Electrical Systems Design</li>
 </ul>
         </li>
 </ul>
@@ -54,33 +52,35 @@ Graduated with 8 GCSE A*-C, including Maths, English, and Science.
 </section>
 <h2 class="subtitle2">Experience</h2>    
 <section class="content_body dark-wrapper"> 
-<span class="subtitle3">UNION ASSISTANT | KENT STUDENT UNION | 25/05/2018 - PRESENT</span>
+<span class="subtitle3">UNION ASSISTANT | KENT STUDENT UNION | April 2018 - PRESENT</span>
 <p>
-    In the retail sector of Kent Union’s non-profit shop on campus, I work independently and manage my own time to get tasks done within the frame of my shift. Liaising with managers and supervisors on a regular basis to ensure they are kept up to date and can organise the other staff where needed. 
+In the retail sector of Kent Union’s non-profit shop on campus, I work independently and manage my own time to get tasks done efficiently and to a high standard. I liaise with managers and supervisors on a regular basis to ensure they are kept up to date on my work to ensure everyone is working towards the same goal. 
 </p>
-<span class="subtitle3">STOCK CONTROL OPERATOR | TESCO | 18/06/2018 - 09/09/2018</span>
+<span class="subtitle3">STOCK CONTROL OPERATOR | TESCO | June 2018 - Sept 2018</span>
 <p>
-This role tested my ability to work under pressure and prioritise tasks throughout the day. Time management was key to success as each section of the store had to be searched and reduced if needed within a small timeframe. Concise and effective communication with the section staff was also required so that they can efficiently replenish stock. 
+This role tested my ability to work under pressure and prioritise tasks throughout the day. Time management was key to success, as each section of the store had to be searched and reduced when required within a small timeframe. Concise and effective communication with the section staff was also required so that they can efficiently replenish stock.
 </p>
-<span class="subtitle3">DOMESTIC ELECTRICIAN | INDEPENDENT | 09/10/15 - 03/01/17</span>
+<span class="subtitle3">DOMESTIC ELECTRICIAN | INDEPENDENT | Oct 2015 - Jan 2017</span>
 <p>
-    While also working at Waitrose, as a newly fully qualified electrician I worked in teams of all sizes alongside people of all trades, some self-employed, some contracted. From wiring houses to preparing trunking and conduit. This role taught me how to communicate and translate terminology from detailed designs to those who are less knowledgeable of the schematics. Presenting schedules to builders allowing us to work simultaneously and not interfere with each other. I have gained experience in how professional teams work in the trades industry from these experiences, which I can transfer to the office.
+While also working at Waitrose, as a newly fully qualified electrician I worked in teams of all sizes alongside people of many trades, some self-employed, some contracted. From wiring houses to preparing trunking and conduit. This role taught me how to communicate and translate terminology from detailed designs to plain English, to those who are less knowledgeable of the schematics. Presenting time schedules to builders, allowing us to work simultaneously and not interfere with each other. I have gained many valuable transferable skills from my time as an electrician of which I look forward to bringing with me to my placement.
 </p>
-<span class="subtitle3">RETAL ASSISTANT | WAITROSE | 19/08/13 - 28/08/18</span>
+<span class="subtitle3">RETAL ASSISTANT | WAITROSE | Aug 2013 - Aug 2018</span>
 <p>
-    Within Waitrose supermarket at Sudbury I worked as a part-time Ambient partner. This customer facing role required me to manage my electrical training and career with shift commitments. Building a good rapport with regular customers and co-workers, allowing me to resolve conflicts within the team easier and help regular customers faster by understanding them as a person. When leading the team, I used initiative and experience to make decisions on how our team can work more efficiently by deciding what needed to be prioritised and how to approach the tasks. I also worked on Customer Services when customer demand required, using the checkout to finalize customers shopping and payments or assist in customer queries.
+Within Waitrose supermarket at Sudbury I worked as a part-time Ambient partner. This customer facing role required me to balance my electrical training and career with shift commitments. Building a good rapport with regular customers and co-workers, allowing me to resolve conflicts within the team easier and help regular customers faster by understanding them as a person. When supervising the team, I used initiative and experience to make decisions on how our team can work more efficiently by listening to others and deciding what needed to be prioritised. I also worked on Customer Services when demand required, using the checkout to finalize customers shopping and payments, or assist in customer queries.
 </p>
 </section>
 <h2 class="subtitle2">Interests</h2>    
 <section class="content_body dark-wrapper">
 <span class="subtitle3">SPORTS</span>
 <p>
-My sporty technical skills come from having good hand eye coordination, at least, when I have my contact lenses in. I play field hockey at university in the sweeper position, I am the last defender before the goal, focusing on the ball and controlling the defenders in front of me is my role for 70 minutes. Table-tennis and Pool are some other activities that I enjoy. 
+My sport ability comes only from having good hand eye coordination, at least, when I have my contact lenses in! I play field hockey at university in the sweeper position, I am the last defender before the goal, focusing on the ball and controlling the defenders in front of me is my role for 70 minutes. Table-tennis and Pool are some other activities that I enjoy in my spare-time.
 </p>
 <span class="subtitle3">TECHNOLOGY</span>
 <p>
-	Competing in online games, rivalled by my older brothers with the motivation to be better than them, which I did. I joined an online community in 2012, taking on a leadership role, setting up our Linux Counter-Strike servers, organising events, and assisting in testing new python plugins before they were introduced to the public.
-	I also enjoy watching a variety of channels on twitch, seeing how other people’s interests and creativity can create content for others. Even learning how things are created and watching people code in programming languages I have never used, which inspired me to use C# in ASP.NET to create a simple interview webpage for my community when I was younger.
+Competing in online games from a young age, rivalled by my older brothers, with the motivation to be better than them, which I did. I joined an online community in 2012, taking on a leadership role, setting up our Linux Counter-Strike servers, organising events, and assisting in testing new python plugins before they were introduced to the public.
+</p>
+<p>
+I enjoy watching a variety of content on Twitch, seeing other people’s interests and creativity create enjoyable content. Learning how things are made, crafts, and watching people code, which was my inspiration to use C# in ASP.NET to create a simple interview webpage for my community before I even had the goal of becoming a software developer.
 </p>
 </section>
 </div>
