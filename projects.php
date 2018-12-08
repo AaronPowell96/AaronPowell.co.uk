21240f0e7e58c25c3985b3a2749f699c9f11afb6
Aaron Powell <Apowell829@gmail.com>
2018-12-08 17:22:47 +0000

    updated project content

--- projects.php
+++ projects.php
@@ -7,18 +7,16 @@
         </div>
         <div class = "projectContent content"> 
                 <h2 class = "subtitle2">My Projects</h2>
-                <p class = "content_body light-wrapper">
+                <section class = "content_body light-wrapper">
                     Here you can find some of the projects I have started, some finished, some still in the works!
-                    I use my projects to test new technologies that I have learned about, to gain a deeper understanding 
-                    of certain concepts and to fulfill my desire to learn.
-                    You can click on the images of each project and view more details, or go to the GitHub page to view the code first-hand. Some projects
-                    may not be added or fully developed yet as the website is still under construction. I want to make sure the website has full functionality before
-                    adding all of the content. Thanks for looking!
-                </p>
+                    <p>I use my projects to test new technologies that I have learned about, to gain a deeper understanding 
+                    of certain concepts and to fulfill my desire to learn. </p>
+                   <p> You can click on the images of each project and view more details, or go to the GitHub page to view the code first-hand.</p><p> Some projects
+                    may not be clickable, added, or be fully developed yet as the website is still under construction. I want to make sure the website has full functionality before
+                    adding all of the content. Thanks for looking!</p>
+</section>
                 <div class = "project_div">
-                    <h2 class = "subtitle2"></h2>
-                    <h2 class = "subtitle3">Cough furball into food bowl then scratch owner for a new one i bet my nine lives</h2>
-                    <p class = "content_body">Cough furball into food bowl then scratch owner for a new one i bet my nine lives</p>
+                    <h2 class = "subtitle2">Click to view each project further.</h2>
                     <div class = "project_image_container">
                     <div class="polaroid"><button class="project-button-image modalButton" data-modal="#memoryGameProject"><img src="assets/images/projects/memoryGameLive.png">
                     <p class="subtitle2">Memory Game</p></button></div>
