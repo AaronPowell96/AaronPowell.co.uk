05ce4fd8c4653a3c79acb4a6d48e525bea53a429
Aaron Powell <Apowell829@gmail.com>
2018-12-08 15:03:44 +0000

    Updated MemoryGame modal and image

--- projects.php
+++ projects.php
@@ -20,6 +20,8 @@
                     <h2 class = "subtitle3">Cough furball into food bowl then scratch owner for a new one i bet my nine lives</h2>
                     <p class = "content_body">Cough furball into food bowl then scratch owner for a new one i bet my nine lives</p>
                     <div class = "project_image_container">
+                    <div class="polaroid"><button class="project-button-image modalButton" data-modal="#project1"><img src="assets/images/projects/memoryGameLive.png">
+                    <p class="subtitle2">Memory Game</p></button></div>
                     <div class="polaroid"><button class="project-button-image modalButton" data-modal="#project1"><img src="assets/images/sudbury.jpg">
                     <p class="subtitle2">Cinque Terre</p></button></div>
                     <div class="polaroid"><button class="project-button-image modalButton" data-modal="#project1"><img src="assets/images/sudbury.jpg">
@@ -27,8 +29,6 @@
                     <div class="polaroid"><button class="project-button-image modalButton" data-modal="#project1"><img src="assets/images/sudbury.jpg">
                     <p class="subtitle2">Cinque Terre</p></button></div>
                     <div class="polaroid"><button class="project-button-image modalButton" data-modal="#project1"><img src="assets/images/sudbury.jpg">
-                    <p class="subtitle2">Cinque Terre</p></button></div>
-                    <div class="polaroid"><button class="project-button-image modalButton" data-modal="#project1"><img src="assets/images/profile_pic.jpg">
                     <p class="subtitle2">Cinque Terre</p></button></div>
 </div>
                 </div>
